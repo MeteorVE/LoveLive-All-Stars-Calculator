60c1b4e13ff1ec1613c1c4dccee1e89735c4c4ef
MeteorV <ekids12345@gmail.com>
2019-10-06 22:18:00 +0800

    first push

--- js/main.js
+++ js/main.js
@@ -5,33 +5,42 @@ function updatePtInput(){
 }
 
 function updateResult(){
-    var reward_target = document.getElementById("pt-need").value;
+    var reward_target = Number(document.getElementById("pt-need").value);
     if (reward_target=="")
         reward_target = 0
-    var acc_pt = document.getElementById("acc-pt").value;
+    var acc_pt = Number(document.getElementById("acc-pt").value);
     if(acc_pt == "")
         acc_pt = 0;
     else
         acc_pt = acc_pt
     
-    var times = Math.ceil((reward_target - acc_pt)/document.getElementById("score_value").textContent);
-    var lp_per = document.getElementById("lp_value").textContent;
-    var extra_lp = lp_per * times - document.getElementById("available_lp").textContent;
+    var score_value = Number(document.getElementById("score_value").textContent);
+    var times = Math.ceil((reward_target - acc_pt) / score_value);
+    var available_lp =  Number(document.getElementById("available_lp").textContent);
+    var lp_per = Number(document.getElementById("lp_value").textContent);
+    var extra_lp = lp_per * times - available_lp;
+    var normal_get_pt = (available_lp / lp_per) * score_value + acc_pt;
     // <h2>計算結果 : </h2><p>PT 還差 XXX，需要 K 場<br>自然回體可打 L 場<br>需要額外 N 管體 = 幾顆星星</p>
     
-    var str = "<h2>計算結果 : </h2><p>PT 還差 " + (reward_target - acc_pt) + " (" + times + "場)<br>需要 " +
-        lp_per * times + " 體力，自然回體有 " + document.getElementById("available_lp").textContent + " 體<br>" +
+    var str = "<h2>計算結果 : </h2><p>自然回體到結束 PT 可達到 " + normal_get_pt +
+        "。<br>PT 還差 " + (reward_target - acc_pt) + " (" + times + "場)<br>需要 " +
+        lp_per * times + " 體力，自然回體有 " + available_lp + " 體<br>" +
         "故需額外 " + extra_lp + " 體力 (" + Math.ceil(extra_lp/100) + " 顆石頭)";
-    str += "<p>額外體力若是負的，代表達標。<br>本服務僅提供試算，不同計算方式會有些差異。<br>有其他建議歡迎下方聯繫討論 :)</p>"
+    str += "<p>額外體力若是負的，代表達標。<br>本服務僅提供試算，不同計算方式會有些差異。\
+            <br>有其他建議歡迎下方聯繫討論 :)</p>"
     document.getElementById("result").innerHTML = str;
 }
 
-function updateLp(){
-    document.getElementById("available_lp").innerHTML = getLp();
+function getHardcoreLp(_countDownDate){
+    var countDownDate = _countDownDate;    
+    var now = new Date();
+    var total_minutes = (countDownDate.getTime() - now.getTime()) / 1000 / 60;
+    console.log( (countDownDate.getTime() - now.getTime())/1000/60/60,"小時" );
+    return Math.floor(total_minutes/3)
 }
 
-function getLp(){
-    var countDownDate = new Date("Oct 15, 2019 13:59:00");    
+function getNormalLp(_countDownDate){
+    var countDownDate = _countDownDate;    
     var now = new Date();
     // now = new Date("Oct 13, 2019 00:00:00");
     
@@ -43,18 +52,60 @@ function getLp(){
         // 幾點起床
         var wakeup_time = document.getElementById("work").value;
         var rest_time = document.getElementById("rest").value;
+        var lp_value = document.getElementById("lp_value").textContent;
         if(wakeup_time == "")
             wakeup_time = 8
         if(rest_time == "")
             rest_time = 23
-        var last_day_minutes = (countDownDate.getHours() - wakeup_time) * 60 + countDownDate.getMinutes();
-        var full_day = (countDownDate.getDate() - now.getDate() - 1)*(rest_time-wakeup_time)*60;
-        var today_minutes = (rest_time - now.getHours()) * 60 - now.getMinutes();
-        console.log("last_day_minutes",last_day_minutes, " full:", full_day, ",  td min:" ,today_minutes);
-        
-        return Math.floor((last_day_minutes + full_day + today_minutes) / 3) + 100 * (countDownDate.getDate() - now.getDate())
-    }
 
+        var last_day_minutes = (countDownDate.getHours() - wakeup_time) * 60 + countDownDate.getMinutes();
+        var full_day = (countDownDate.getDate() - now.getDate() - 2)*( (rest_time-wakeup_time)*60 + 300 ) ; // +300 因為睡覺回體
+        var today_minutes = (rest_time - now.getHours()) * 60 - now.getMinutes();
+        var true_lp = Math.floor((last_day_minutes + full_day + today_minutes) / 3) + 100 * (countDownDate.getDate() - now.getDate());
+        var total_times = Math.floor((today_minutes / 3) / lp_value) + Math.floor((last_day_minutes / 3) / lp_value) + Math.floor((full_day/3)/lp_value);
+        console.log("last_day_minutes : ",last_day_minutes, " full_day_mins : ", full_day, ",  today remain mins :" ,today_minutes);
+        //console.log(countDownDate.getDate() - now.getDate() - 2, (rest_time - wakeup_time) * 60 + 300);
+        return total_times * lp_value
+    }
+}
+
+function getCasualLp(_countDownDate){
+    var countDownDate = _countDownDate;
+    var now = new Date();
+    // now = new Date("Oct 13, 2019 00:00:00");
+    
+    if (now.getDate() == countDownDate.getDate()) {
+        // 5hr = 100LP, 60min = 20LP, 3min = 1LP
+        var minutes = (countDownDate.getHours() - now.getHours()) * 60 + countDownDate.getMinutes() - now.getMinutes();
+        return Math.floor(minutes / 3) // 除三因為 3min  = 1LP
+    } else {
+        var wakeup_time = document.getElementById("work").value;
+        var rest_time = document.getElementById("rest").value;
+        if (wakeup_time == "")
+            wakeup_time = 8
+        if (rest_time == "")
+            rest_time = 23
+        var lp_value = document.getElementById("lp_value").textContent;
+
+        var last_day_minutes = (countDownDate.getHours() - wakeup_time) * 60 + countDownDate.getMinutes();
+        var full_day_times = (countDownDate.getDate() - now.getDate() - 2);
+        var today_minutes = ((rest_time - now.getHours()) * 60 - now.getMinutes()) < 300 ? (rest_time - now.getHours()) * 60 - now.getMinutes() : 300;
+        // console.log("last_day_minutes", last_day_minutes, " full:", full_day, ",  td min:", today_minutes);
+        var total_times = Math.floor((today_minutes / 3) / lp_value) + Math.floor(100 / lp_value) * 2 * full_day_times + Math.floor((last_day_minutes / 3) / lp_value)
+        console.log(Math.floor((last_day_minutes / 3) / lp_value));
+        
+        return total_times * lp_value
+    }
+}
+
+function updateAvailableLp(countDownDate) {
+    var checked_btn = document.querySelector("input[name='player-options']:checked");
+    if (checked_btn.id == 'casual')
+        document.getElementById("available_lp").innerHTML = getCasualLp(countDownDate);
+    else if (checked_btn.id == 'hardcore')
+        document.getElementById("available_lp").innerHTML = getHardcoreLp(countDownDate); 
+    else
+        document.getElementById("available_lp").innerHTML = getNormalLp(countDownDate);
 }
 
 function updateLevelDescription(_inBtn){
@@ -78,7 +129,7 @@ function updateLevelDescription(_inBtn){
 
 function updatePlayerDescription(){
     var checked_btn = document.querySelector("input[name='player-options']:checked");
-    var player_dir = [{ 'type': 'casual', 'description': "一天只登起床睡前兩次，共耗體 200體/天"},
+    var player_dir = [{ 'type': 'casual', 'description': "一天只登起床睡前兩次，共耗體 200體/天<br>此選項計算可能會有誤差。"},
         { 'type': 'normal', 'description': "早8 ~ 晚11的 15小時 都不爆體，共耗體 400體/天" },
         { 'type': 'hardcore', 'description': "每5個小時就清一次體力，活動間不爆體，共耗體 480體/天" }
         ];
@@ -89,11 +140,9 @@ function updatePlayerDescription(){
             break;
         }
     }
-    
-    // var distance = countDownDate - now;
-    
     document.getElementById("player-description").innerHTML = str;
 }
+
 
 function init(){
     var level_labels = document.querySelectorAll("#level>label");
@@ -102,12 +151,16 @@ function init(){
     var rewardSelect = document.getElementById("rewardSelect");
     var acc_pt = document.getElementById("acc-pt");
 
+    var countDownDate = new Date("Oct 15, 2019 13:59:00");    
+    updateAvailableLp(countDownDate);
+
     for(var la of level_labels){
         la.addEventListener('click', function () {
             // this === link
             // console.log(this.getElementsByTagName('input')[0]);
             setTimeout(function () { 
                 updateLevelDescription(); 
+                updateAvailableLp(countDownDate);
                 updateResult();}, 500);  
             // updateLevelDescription(this.getElementsByTagName('input')[0]);
             
@@ -128,8 +181,8 @@ function init(){
         la.addEventListener('click', function () {
             // this === link
             setTimeout(function () { 
-                updateLp();
                 updatePlayerDescription();
+                updateAvailableLp(countDownDate);
                 updateResult();
             }, 500);
             
@@ -159,7 +212,6 @@ function init(){
 
 }
 
-init();
-updateLp();
 updateLevelDescription();
 updatePlayerDescription();
+init();
--- js/timercounter.js
+++ js/timercounter.js
@@ -19,8 +19,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-    document.getElementById("timer_now").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+    document.getElementById("timer_now").innerHTML = days + " 天 " + hours + " 小時 "
+  + minutes + " 分 " + seconds + " 秒 ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
