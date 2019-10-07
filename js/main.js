6af05d85ad1ddf6593b015a6a7126a4dfc0d7da8
MeteorV <ekids12345@gmail.com>
2019-10-08 01:56:13 +0800

    add exp compute

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 
-function computeExp(){
-    UP_EXP = [0, 100, 115, 130, 145, 190, 240, 285, 335, 385, 435, 485, 535, 590, 640, 690, 745, 800, 850, 905, 960, 1015, 1065, 1120, 1175, 1230, 1285, 1345, 1400, 1455, 1510, 1570, 1625, 1680, 1740, 1795, 1850, 1910, 1970, 2025, 2085, 2140, 2200, 2260, 2315, 2375, 2435, 2495, 2550, 2610, 2670, 2730, 2790, 2850, 2910, 2970, 3030, 3090, 3150, 3210, 3270, 3330, 3390, 3450, 3510, 3575, 3635, 3695, 3755, 3815, 3880, 3940, 4e3, 4065, 4125, 4185, 4250, 4310, 4370, 4435, 4495, 4560, 4620, 4685, 4745, 4810, 4870, 4935, 4995, 5060, 5125, 5185, 5250, 5310, 5375, 5440, 5500, 5565, 5630, 5690, 5755, 5820, 5885, 5945, 6010, 6075, 6140, 6205, 6265, 6330, 6395, 6460, 6525, 6590, 6655, 6715, 6780, 6845, 6910, 6975, 7040, 7105, 7170, 7235, 7300, 7365, 7430, 7495, 7560, 7625, 7690, 7755, 7820, 7885, 7955, 8020, 8085, 8150, 8215, 8280, 8345, 8415, 8480, 8545, 8610, 8675, 8745, 8810, 8875, 8940, 9005, 9075, 9140, 9205, 9275, 9340, 9405, 9470, 9540, 9605, 9670, 9740, 9805, 9875, 9940, 10005, 10075, 10140, 10205, 10275, 10340, 10410, 10475, 10545, 10610, 10675, 10745, 10810, 10880, 10945, 11015, 11080, 11150, 11215, 11285, 11350, 11420, 11490, 11555, 11625, 11690, 11760, 11825, 11895, 11965, 12030, 12100, 12165, 12235, 12305];
+function computeExp(_times){
+    var current_exp = document.getElementById("current_exp").value;
+    var current_lv = document.getElementById("current_lv").value;
+    if (current_lv == ""){
+        console.log("ERROR");
+        current_lv = 1;
+    }
+    if(current_exp == ""){
+        current_exp = 0;
+    }
+
+    var exp_per_song = [16,27,42];
+    var total_exp = current_exp;
+    var UP_EXP = [0, 100, 115, 130, 145, 190, 240, 285, 335, 385, 435, 485, 535, 590, 640, 690, 745, 800, 850, 905, 960, 1015, 1065, 1120, 1175, 1230, 1285, 1345, 1400, 1455, 1510, 1570, 1625, 1680, 1740, 1795, 1850, 1910, 1970, 2025, 2085, 2140, 2200, 2260, 2315, 2375, 2435, 2495, 2550, 2610, 2670, 2730, 2790, 2850, 2910, 2970, 3030, 3090, 3150, 3210, 3270, 3330, 3390, 3450, 3510, 3575, 3635, 3695, 3755, 3815, 3880, 3940, 4e3, 4065, 4125, 4185, 4250, 4310, 4370, 4435, 4495, 4560, 4620, 4685, 4745, 4810, 4870, 4935, 4995, 5060, 5125, 5185, 5250, 5310, 5375, 5440, 5500, 5565, 5630, 5690, 5755, 5820, 5885, 5945, 6010, 6075, 6140, 6205, 6265, 6330, 6395, 6460, 6525, 6590, 6655, 6715, 6780, 6845, 6910, 6975, 7040, 7105, 7170, 7235, 7300, 7365, 7430, 7495, 7560, 7625, 7690, 7755, 7820, 7885, 7955, 8020, 8085, 8150, 8215, 8280, 8345, 8415, 8480, 8545, 8610, 8675, 8745, 8810, 8875, 8940, 9005, 9075, 9140, 9205, 9275, 9340, 9405, 9470, 9540, 9605, 9670, 9740, 9805, 9875, 9940, 10005, 10075, 10140, 10205, 10275, 10340, 10410, 10475, 10545, 10610, 10675, 10745, 10810, 10880, 10945, 11015, 11080, 11150, 11215, 11285, 11350, 11420, 11490, 11555, 11625, 11690, 11760, 11825, 11895, 11965, 12030, 12100, 12165, 12235, 12305];
+    var diff_btn = document.querySelector("input[name='level-options']:checked");
+    if (diff_btn.id.search("hard") != -1){
+        total_exp = exp_per_song[2];
+    } else if (diff_btn.id.search("normal") != -1){
+        total_exp = exp_per_song[1];
+    }else{
+        total_exp = exp_per_song[0];
+    }
+    total_exp = _times*total_exp;
+    //console.log("Total exp : ", total_exp);
+    
+    var cnt = 0;
+    while( total_exp > 0 ){
+        total_exp = total_exp - UP_EXP[current_lv++];
+        cnt++;
+    }
+    console.log("level up times:", cnt - 1, " now level:", current_lv - 1, " now your exp", total_exp + UP_EXP[current_lv-1]);
+    return { "level_up_times": cnt - 1, "now_level": current_lv - 1, "now_your_exp": total_exp + UP_EXP[current_lv - 1]}
 }
 
 function updatePtInput(){
@@ -24,12 +54,23 @@ function updateResult(){
     var lp_per = Number(document.getElementById("lp_value").textContent);
     var extra_lp = lp_per * times - available_lp;
     var normal_get_pt = (available_lp / lp_per) * score_value + acc_pt;
-    // <h2>計算結果 : </h2><p>PT 還差 XXX，需要 K 場<br>自然回體可打 L 場<br>需要額外 N 管體 = 幾顆星星</p>
     
+
+    let need_star = Math.ceil(extra_lp / 10);
+    // <h2>計算結果 : </h2><p>PT 還差 XXX，需要 K 場<br>自然回體可打 L 場<br>需要額外 N 管體 = 幾顆星星</p>
     var str = "<h2>計算結果 : </h2><p>自然回體到結束 PT 可達到 " + normal_get_pt +
         "。<br>PT 還差 " + (reward_target - acc_pt) + " (" + times + "場)<br>需要 " +
         lp_per * times + " 體力，自然回體有 " + available_lp + " 體<br>" +
-        "故需額外 " + extra_lp + " 體力 (" + Math.ceil(extra_lp/10) + " 顆石頭)";
+        "故需額外 " + extra_lp + " 體力 (" + need_star + " 顆石頭)";
+
+    var expDir = {}
+    if (document.getElementById("current_lv").value != ""){
+        expDir = computeExp(times);
+        console.log(expDir);
+        
+        str += "<br>預估會到 LV" + expDir['now_level'] + ", 升級次數 " + expDir['level_up_times'] + " 次(仍需" + (need_star-expDir['level_up_times']*10) +"顆)";
+    }
+
     str += "<p>額外體力若是負的，代表達標。<br>本服務僅提供試算，不同計算方式會有些差異。\
             <br>有其他建議歡迎下方聯繫討論 :)</p>"
     document.getElementById("result").innerHTML = str;
@@ -214,6 +255,13 @@ function init(){
         updateResult();
     })
 
+    document.getElementById("current_lv").addEventListener('input', function () {
+        updateResult();
+    })
+
+    document.getElementById("current_exp").addEventListener('input', function () {
+        updateResult();
+    })
 }
 
 updateLevelDescription();
